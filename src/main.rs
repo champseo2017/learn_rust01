83092e40c73d6e647f2a388af2babdb520dc32e1
Chaun Ouamthip <32265715+champseo2017@users.noreply.github.com>
2025-07-12 23:15:46 +0700

    method สุ่มเลข

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-// นำ io library เข้ามาใน scope เพื่อให้สามารถใช้งานฟังก์ชันด้าน input/output ได้
 use std::io;
+use rand::Rng; // import Rng trait เข้ามาใน scope เพื่อให้ใช้ method สุ่มเลขได้
 
-// function `main` คือจุดเริ่มต้นของโปรแกรม
 fn main() {
-    // print ข้อความต้อนรับและบอกให้ user ใส่ตัวเลขที่เดา
     println!("Guess the number!");
+
+    // สร้างตัวแปรเก็บเลขลับ โดยเรียกใช้ generator แล้วสุ่มเลขในช่วง 1 ถึง 100
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
-    // สร้างตัวแปรแบบ mutable (แก้ไขค่าได้) ชื่อ guess เพื่อเก็บค่าที่ user พิมพ์
-    // โดยกำหนดให้เป็น String เปล่าๆ ในตอนแรก
     let mut guess = String::new();
 
-    // อ่านค่าที่ user พิมพ์จาก standard input (keyboard)
     io::stdin()
-        .read_line(&mut guess) // อ่านค่าที่ user พิมพ์มาทั้งบรรทัดแล้วเก็บไว้ในตัวแปร guess
-        .expect("Failed to read line"); // จัดการกับ error ที่อาจเกิดขึ้นหากอ่านค่าไม่สำเร็จ
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    // print ค่าที่ user เดาออกมา โดยใช้ตัวแปร guess ที่รับค่ามาแล้ว
     println!("You guessed: {guess}");
 }
